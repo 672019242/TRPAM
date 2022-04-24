9621736b0c088226530de61a067ef7ab311e143b
unknown <you@example.com>
2022-04-25 00:01:58 +0700

    tr

--- app/src/main/java/com/example/trpam/MainActivity.java
+++ app/src/main/java/com/example/trpam/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.trpam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/trpam/RegisterUser.java
@@ -0,0 +1,20 @@
+package com.example.trpam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuthException;
+
+public class RegisterUser extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+
+    }
+}
